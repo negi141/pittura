48db324a90c56a3ab2581667b0e3602da8690370
Negishi <takuto.negishi@gmail.com>
2020-03-30 19:55:28 +0900

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  get 'works/index'
   get 'works/select'
   get 'works/draw'
+  get 'works/list'
+  get '/w/:id', to: 'works#view'
   
   root 'works#index'
 
